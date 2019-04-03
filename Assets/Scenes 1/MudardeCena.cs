b155e7be31de1c6ffd05ef3bfd8e1330d2b84468
unknown <viniciusbustamanti@gmail.com>
2019-04-03 15:52:51 -0300

    Adiciona Menu

---
+++ Assets/Scenes 1/MudardeCena.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MudardeCena : MonoBehaviour
+{
+    public string Cena;
+    public void ChangeScene()
+    {
+        Application.LoadLevel(Cena);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Player"){
+            StartCoroutine(PlzWaitSir());
+        }
+    }
+    
+    IEnumerator PlzWaitSir()
+    {
+        print("changing scene...");
+        yield return new WaitForSeconds(0.5f);
+        ChangeScene(); 
+    }
+
+}
